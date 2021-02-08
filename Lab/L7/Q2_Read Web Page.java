61be3813ca87ca0c1ee07c09eb165b77d5a614ad
Norhidayah <75527623+hidayahRB@users.noreply.github.com>
2021-02-08 12:53:21 +0800

    Create Q2_Read Web Page.java

---
+++ Lab/L7/Q2_Read Web Page.java
@@ -0,0 +1,37 @@
+package lab7;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+public class Q2 {
+
+    public static void main(String[] args) {
+        // Write the contents of the Web page into a text file name index.htm.
+        
+        try {
+            URL u = new URL("https://fsktm.um.edu.my/");
+            URLConnection cnn = u.openConnection();
+            InputStream stream = cnn.getInputStream();
+            Scanner in = new Scanner(stream);
+
+            PrintWriter outputStream = new PrintWriter (new FileOutputStream("F:\\document\\FOP_SEM1\\Files\\index.htm"));
+            while(in.hasNextLine()){
+                String line = in.nextLine();
+                String [] words = line.split(" ");
+                for(int i = 0 ; i < words.length ; i++){
+
+                }
+                outputStream.println(in.nextLine());
+            }
+
+            outputStream.close();
+        } catch (IOException e) {
+            System.out.println("IO Error:" + e.getMessage());
+        }
+    }   
+}
